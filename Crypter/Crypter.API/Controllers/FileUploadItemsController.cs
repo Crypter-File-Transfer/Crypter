286c83b071654cc47e0d803591b2dc1ed0d56aef
Jack-Edwards <jackedwards@protonmail.com>
2021-04-07 12:02:56 -0500

    Add API project to the solution

---
+++ Crypter/Crypter.API/Controllers/FileUploadItemsController.cs
@@ -0,0 +1,95 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CrypterAPI.Models;
+
+namespace CrypterAPI.Controllers
+{
+   [Route("api/[controller]")]
+   //[ApiController]
+   public class FileUploadItemsController : ControllerBase
+   {
+      public CrypterDB Db { get; }
+
+      public FileUploadItemsController(CrypterDB db)
+      {
+         Db = db;
+      }
+
+      // POST: api/FileUploadItems
+      // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+      [HttpPost]
+      public async Task<IActionResult> FileUploadItem([FromBody] FileUploadItem body)
+      {
+         await Db.Connection.OpenAsync();
+         body.Db = Db;
+         await body.InsertAsync();
+         return new OkObjectResult(body);
+      }
+
+      // GET: api/FileUploadItems
+      [HttpGet]
+      public async Task<IActionResult> FileUploadItems()
+      {
+         await Db.Connection.OpenAsync();
+         var query = new FileUploadItemQuery(Db);
+         var result = await query.LatestItemsAsync();
+         return new OkObjectResult(result);
+      }
+
+      // GET: api/FileUploadItems/5
+      [HttpGet("{id}")]
+      public async Task<IActionResult> GetFileUploadItem(int id)
+      {
+         await Db.Connection.OpenAsync();
+         var query = new FileUploadItemQuery(Db);
+         var result = await query.FindOneAsync(id);
+         if (result is null)
+            return new NotFoundResult();
+         return new OkObjectResult(result);
+      }
+
+      // PUT: api/FileUploadItems/5
+      // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+      [HttpPut("{id}")]
+      public async Task<IActionResult> PutFileUploadItem(int id, [FromBody] FileUploadItem body)
+      {
+         await Db.Connection.OpenAsync();
+         var query = new FileUploadItemQuery(Db);
+         var result = await query.FindOneAsync(id);
+         if (result is null)
+            return new NotFoundResult();
+         //update fields
+         result.UntrustedName = body.UntrustedName;
+         result.UserID = body.UserID;
+         result.Size = body.Size;
+         result.FileContent = body.FileContent;
+         await result.UpdateAsync();
+         return new OkObjectResult(result);
+
+      }
+
+      // DELETE: api/FileUploadItems/5
+      [HttpDelete("{id}")]
+      public async Task<IActionResult> DeleteFileUploadItem(int id)
+      {
+         await Db.Connection.OpenAsync();
+         var query = new FileUploadItemQuery(Db);
+         var result = await query.FindOneAsync(id);
+         if (result is null)
+            return new NotFoundResult();
+         await result.DeleteAsync();
+         return new OkResult();
+      }
+
+      // Requires safe updates to be disabled within MySQl editor preferences
+      // DELETE api/FileUploadItems
+      [HttpDelete]
+      public async Task<IActionResult> DeleteAll()
+      {
+         await Db.Connection.OpenAsync();
+         var query = new FileUploadItemQuery(Db);
+         await query.DeleteAllAsync();
+         return new OkResult();
+      }
+   }
+}
---
+++ Crypter/Crypter.API/Models/TextUploadItem.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using MySqlConnector;
+namespace CrypterAPI.Models
+{
+   //TextUpload inherits from UploadItem
+   public class TextUploadItem : UploadItem
+   {
+      //add additional members/ methods unique to text uploads
+      public string CharCount { get; set; }
+      public string Message { get; set; }
+      //constructor sets TimeStamp upon instantiation
+      public TextUploadItem()
+      {
+         this.TimeStamp = DateTime.UtcNow;
+      }
+      internal TextUploadItem(CrypterDB db)
+      {
+         Db = db;
+      }
+      public async Task InsertAsync()
+      {
+         using var cmd = Db.Connection.CreateCommand();
+         cmd.CommandText = @"INSERT INTO `TextUploadItems` (`UntrustedName`, `UserID`, `Size`, `TimeStamp`, `CharCount`, `Message`) VALUES (@untrustedname, @userid, @size, @timestamp, @charcount, @message);";
+         BindParams(cmd);
+         await cmd.ExecuteNonQueryAsync();
+         Id = (int)cmd.LastInsertedId;
+      }
+
+      public async Task UpdateAsync()
+      {
+         using var cmd = Db.Connection.CreateCommand();
+         cmd.CommandText = @"UPDATE `TextUploadItems` SET `UntrustedName` = @untrustedname, `UserID` = @userid, `Size` = @size, `TimeStamp` = @timestamp, `CharCount` = @charcount, `Message`=@message WHERE `Id` = @id;";
+         BindParams(cmd);
+         BindId(cmd);
+         await cmd.ExecuteNonQueryAsync();
+      }
+
+      public async Task DeleteAsync()
+      {
+         using var cmd = Db.Connection.CreateCommand();
+         cmd.CommandText = @"DELETE FROM `TextUploadItems` WHERE `Id` = @id;";
+         BindId(cmd);
+         await cmd.ExecuteNonQueryAsync();
+      }
+
+      private void BindId(MySqlCommand cmd)
+      {
+         cmd.Parameters.Add(new MySqlParameter
+         {
+            ParameterName = "@id",
+            DbType = DbType.Int32,
+            Value = Id,
+         });
+      }
+
+      private void BindParams(MySqlCommand cmd)
+      {
+         cmd.Parameters.Add(new MySqlParameter
+         {
+            ParameterName = "@untrustedname",
+            DbType = DbType.String,
+            Value = UntrustedName,
+         });
+         cmd.Parameters.Add(new MySqlParameter
+         {
+            ParameterName = "@userid",
+            DbType = DbType.String,
+            Value = UserID,
+         });
+         cmd.Parameters.Add(new MySqlParameter
+         {
+            ParameterName = "@size",
+            DbType = DbType.String,
+            Value = Size,
+         });
+         cmd.Parameters.Add(new MySqlParameter
+         {
+            ParameterName = "@timestamp",
+            DbType = DbType.String,
+            Value = TimeStamp,
+         });
+         cmd.Parameters.Add(new MySqlParameter
+         {
+            ParameterName = "@charcount",
+            DbType = DbType.Int16,
+            Value = CharCount,
+         });
+         cmd.Parameters.Add(new MySqlParameter
+         {
+            ParameterName = "@message",
+            DbType = DbType.String,
+            Value = Message,
+         });
+      }
+   }
+
+}
