7d07cbae410ca4031a6e2714fe613d7cf5ebc58b
peterst5 <peterst5@oregonstate.edu>
2021-04-13 18:13:46 -0500

    File and Message Upload UI
    Created the UI for the Anonymous File and Message uploads.

--- Crypter.Web/Startup.cs
+++ Crypter.Web/Startup.cs
@@ -23,8 +23,8 @@ namespace Crypter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
             services.AddServerSideBlazor();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
