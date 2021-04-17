f8e752367251291e7bc5a0e1bf780764a06b6cde
saescalante <42281151+saescalante@users.noreply.github.com>
2021-04-17 15:59:43 -0500

    update to appsettings.json files

--- Crypter.API/Models/Key.cs
+++ Crypter.API/Models/Key.cs
@@ -10,7 +10,11 @@ namespace CrypterAPI.Models
         public string UserId { get; set; }
         public string PrivateKey { get; set; }
         public string PublicKey { get; set; }
-        public string KeyType { get; set;  }
+        enum KeyType
+        {
+            Global,
+            Exchanged
+        }
         // file time stamp
         public DateTime Created { get; set; }
         internal CrypterDB Db { get; set; }
--- Crypter.API/Startup.cs
+++ Crypter.API/Startup.cs
@@ -21,16 +21,6 @@ namespace CrypterAPI
       public void ConfigureServices(IServiceCollection services)
       {
          services.AddTransient<CrypterDB>(_ => new CrypterDB(Configuration["ConnectionStrings:DefaultConnection"]));
-
-         //// Adds db context to DI container and specifies context uses in-memory database
-         //services.AddDbContext<UploadItemContext>(opt => opt.UseInMemoryDatabase("CrypterDB"));
-         //services.AddControllers();
-         ////text upload db context
-         //services.AddDbContext<TextUploadItemContext>(opt => opt.UseInMemoryDatabase("CrypterMessagesDB"));
-         //services.AddControllers();
-         //// file upload db context
-         //services.AddDbContext<FileUploadItemContext>(opt => opt.UseInMemoryDatabase("CrypterFileDB"));
-
          services.AddControllers();
       }
 
