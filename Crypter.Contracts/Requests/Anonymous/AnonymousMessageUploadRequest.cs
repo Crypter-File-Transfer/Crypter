4306a9c34d32829696f2f70d42be31a823694c0a
Jack-Edwards <jackedwards@protonmail.com>
2021-04-21 12:14:50 -0500

    Add 'Crypter.Contracts' project to solution

---
+++ Crypter.Contracts/Requests/Anonymous/AnonymousMessageUploadRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Crypter.Contracts.Requests.Anonymous
+{
+    public class AnonymousMessageUploadRequest
+    {
+        public string CipherText { get; set; }
+        public string Signature { get; set; }
+        public string ServerEncryptionKey { get; set; }
+    }
+}
