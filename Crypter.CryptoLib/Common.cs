8b32c3e032c6a20f8ec4b0459e088815c204dcc5
Jack-Edwards <jackedwards@protonmail.com>
2021-04-23 18:57:16 -0500

    Update the UI as encryption progresses

--- Crypter.CryptoLib/Common.cs
+++ Crypter.CryptoLib/Common.cs
@@ -1,50 +1,45 @@
-﻿using Crypter.CryptoLib.BouncyCastle;
+﻿using System;
+using System.Text;
+using Crypter.CryptoLib.BouncyCastle;
 using Crypter.CryptoLib.Enums;
 using Crypter.CryptoLib.Models;
-using System;
-using System.Text;
-using System.Text.Json;
+using Org.BouncyCastle.Crypto;
 
 namespace Crypter.CryptoLib
 {
    public static class Common
    {
-      public static ClientEncryptResult DoAnonymousClientEncryption(byte[] plainBytes, CryptoStrength strength)
+      public static SymmetricCryptoParams GenerateSymmetricCryptoInfo(CryptoStrength strength)
       {
-         // Encipher the plaintext
          var symmetricWrapper = new SymmetricWrapper();
          var aesBlockSize = MapStrengthToAesBlockSize(strength);
          var symmetricKey = symmetricWrapper.GenerateSymmetricKey(aesBlockSize);
          var iv = symmetricWrapper.GenerateIV();
-         var cipherText = symmetricWrapper.EncryptBytes(plainBytes, symmetricKey, iv);
 
-         // Create a signature, which contains a hash of the plaintext and the plaintext public key
-         var digestAlgorithm = MapStrengthToDigestAlgorithm(strength);
-         byte[] messageHash = DigestWrapper.GetDigest(plainBytes, digestAlgorithm);
-         var encodedHash = Convert.ToBase64String(messageHash);
-         var encodedSymmetricKey = Convert.ToBase64String(symmetricKey.ConvertToBytes());
-         var encodedIV = Convert.ToBase64String(iv);
-         var signature = new AnonymousSignature(digestAlgorithm, encodedHash, encodedSymmetricKey, encodedIV);
-         var signatureString = signature.ToString();
-         var signatureBytes = Encoding.UTF8.GetBytes(signatureString);
+         return new SymmetricCryptoParams(symmetricKey, iv);
+      }
 
-         Console.WriteLine($"Signature length: {signatureBytes.Length}");
+      public static byte[] DoSymmetricEncryption(byte[] plaintext, SymmetricCryptoParams symmetricParams)
+      {
+         var symmetricWrapper = new SymmetricWrapper();
+         return symmetricWrapper.EncryptBytes(plaintext, symmetricParams.Key, symmetricParams.IV);
+      }
 
-         // Encipher the signature using an RSA public key
+      public static AsymmetricCipherKeyPair GenerateAsymmetricKeys(CryptoStrength strength)
+      {
          var asymmetricWrapper = new AsymmetricWrapper();
          var rsaKeySize = MapStrengthToRsaKeySize(strength);
-         var asymmetricKeyPair = asymmetricWrapper.GenerateAsymmetricKeyPair(rsaKeySize);
-         var encryptedSignature = asymmetricWrapper.Encrypt(signatureBytes, asymmetricKeyPair.Public);
+         return asymmetricWrapper.GenerateAsymmetricKeyPair(rsaKeySize);
+      }
 
-         // Hash the symmetric key to create a server-side encryption key
-         var hashedSymmetricKey = DigestWrapper.GetDigest(symmetricKey.ConvertToBytes(), DigestAlgorithm.SHA256);
-
-         // Base64 encode everything that needs to be transmitted or displayed
-         var encodedCipherText = Convert.ToBase64String(cipherText);
-         var encodedSignature = Convert.ToBase64String(encryptedSignature);
-         var encodedServerEncryptionKey = Convert.ToBase64String(hashedSymmetricKey);
-
-         return new ClientEncryptResult(encodedCipherText, encodedSignature, encodedServerEncryptionKey, asymmetricKeyPair);
+      public static AnonymousSignature CreateSignature(byte[] plaintext, SymmetricCryptoParams symmetricParams, AsymmetricKeyParameter publicKey, CryptoStrength strength)
+      {
+         var digestAlgorithm = MapStrengthToDigestAlgorithm(strength);
+         byte[] messageHash = DigestWrapper.GetDigest(plaintext, digestAlgorithm);
+         var encodedHash = Convert.ToBase64String(messageHash);
+         var encodedSymmetricKey = Convert.ToBase64String(symmetricParams.Key.ConvertToBytes());
+         var encodedIV = Convert.ToBase64String(symmetricParams.IV);
+         return new AnonymousSignature(digestAlgorithm, encodedHash, encodedSymmetricKey, encodedIV);
       }
 
       public static DigestAlgorithm MapStrengthToDigestAlgorithm(CryptoStrength strength)
---
+++ Crypter.CryptoLib/Models/SymmetricCryptoParams.cs
@@ -0,0 +1,16 @@
+﻿using Org.BouncyCastle.Crypto.Parameters;
+
+namespace Crypter.CryptoLib.Models
+{
+   public class SymmetricCryptoParams
+   {
+      public KeyParameter Key { get; }
+      public byte[] IV { get; }
+
+      public SymmetricCryptoParams(KeyParameter key, byte[] iv)
+      {
+         Key = key;
+         IV = iv;
+      }
+   }
+}
