a283c04d3f3cd7ec9497055a38ff18e5be853530
Jack-Edwards <jackedwards@protonmail.com>
2021-04-27 12:20:40 -0500

    Replace hardcoded URLs with dynamic and configurable URLs

---
+++ Crypter.Web/Models/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Crypter.Web.Models
+{
+    /// <summary>
+    /// Caution!  Do not add secrets here!
+    /// These settings are available to the client.
+    /// </summary>
+    public class AppSettings
+    {
+        public string ApiBaseUrl { get; set; }
+    }
+}
