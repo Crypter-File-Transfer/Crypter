1cbea3395e938a55782e9dea161a41495ababdb1
peterst5 <peterst5@oregonstate.edu>
2021-04-28 21:51:05 -0500

    File Download is implemented
    A file downloads from the page and the interface exists, but it is having issues with anything other than .txt files. I tried for several hours to get it working, but could not.

---
+++ Crypter.Web/wwwroot/js/DecryptDetails.js
@@ -0,0 +1,9 @@
+﻿function saveFile(filename, bytesBase64) {
+    console.log("downloading");
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + bytesBase64;
+    document.body.appendChild(link); // Needed for Firefox
+    link.click();
+    document.body.removeChild(link);
+}
