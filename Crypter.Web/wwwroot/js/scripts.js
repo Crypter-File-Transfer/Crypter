68ae17aaf9799ac510e4a0d1b6d04a3cca57105b
peterst5 <peterst5@oregonstate.edu>
2021-05-05 20:01:53 -0500

    User Settings UI and "Edit" state
    Created the UI for the User Settings including "edit" state

---
+++ Crypter.Web/wwwroot/js/scripts.js
@@ -0,0 +1,17 @@
+﻿function EditAccountDetails() {
+    var fields = document.getElementById("DetailsForm").elements;
+
+    for (var i in fields) {
+        fields[i].readOnly = false;
+        fields[i].disabled = false;
+    }
+}
+
+function SaveAccountDetails() {
+    var fields = document.getElementsByTagName('input');
+
+    for (var i in fields) {
+        fields[i].readOnly = true;
+        fields[i].disabled = true;
+    }
+}
