41e459892b80c9b16d84f5cccce3c6fc7610455e
peterst5 <peterst5@oregonstate.edu>
2021-05-24 18:10:16 -0500

    Updated Login UX
    Updated Login component to use EditForm component
    * Allows for standard form functions (tabbing and enter to submit)
    * Includes "validation" in the form of making sure required fields are filled

---
+++ Crypter.Web/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crypter.Web.Models
+{
+    public class Login
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
