51e29118c119fd444747b72645814eaa14a97d76
peterst5 <peterst5@oregonstate.edu>
2021-05-24 18:22:21 -0500

    Updated Registration UX
    Updated Registration component to use EditForm component
    * Allows for standard form functions (tabbing and enter to submit)
    * Includes "validation" in the form of making sure required fields are filled

---
+++ Crypter.Web/Models/Register.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crypter.Web.Models
+{
+    public class Register
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
