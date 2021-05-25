da9077c09ad254a6f30552c30cf02e00fa74c6bd
peterst5 <peterst5@oregonstate.edu>
2021-05-24 19:23:17 -0500

    Updated Search UX
    Updated Search component to use EditForm component
    * Allows for standard form functions (tabbing and enter to submit)
    * Includes "validation" in the form of making sure required fields are filled

---
+++ Crypter.Web/Models/SearchInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crypter.Web.Models
+{
+    public class SearchInfo
+    {
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public string Query { get; set; }
+
+        public int Index { get; set; }
+
+        public int NumResults { get; set; }
+
+        public int Page { get; set; }
+
+        public SearchInfo()
+        {
+            Index = 0;
+            NumResults = 20;
+            Page = 1;
+        }
+    }
+}
