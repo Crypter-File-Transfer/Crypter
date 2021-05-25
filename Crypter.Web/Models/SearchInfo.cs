107698020cbc1d89c16448ad57c341cdc69d91d1
peterst5 <peterst5@oregonstate.edu>
2021-05-24 19:30:50 -0500

    Added search validation
    Forgot to add the validation bits for the search.

--- Crypter.Web/Models/SearchInfo.cs
+++ Crypter.Web/Models/SearchInfo.cs
@@ -8,6 +8,8 @@ namespace Crypter.Web.Models
         public string Type { get; set; }
 
         [Required]
+        [Display(Name = "Search Term")]
+        [MinLength(2, ErrorMessage = "Search Term must be at least 2 characters")]
         public string Query { get; set; }
 
         public int Index { get; set; }
@@ -18,6 +20,7 @@ namespace Crypter.Web.Models
 
         public SearchInfo()
         {
+            Type = "username";
             Index = 0;
             NumResults = 20;
             Page = 1;
