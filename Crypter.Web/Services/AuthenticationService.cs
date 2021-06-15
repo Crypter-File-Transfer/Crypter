29ee1e276e9401fb2237a2133056400097795f09
Jack-Edwards <jackedwards@protonmail.com>
2021-06-15 17:37:44 -0500

    HttpService also returns HttpStatusCode

--- Crypter.Web/Services/AuthenticationService.cs
+++ Crypter.Web/Services/AuthenticationService.cs
@@ -9,68 +9,70 @@ using System.Text;
 namespace Crypter.Web.Services
 {
 
-    public interface IAuthenticationService
-    {
-        User User { get; }
-        Task Initialize();
-        Task<bool> Login(string username, string plaintextPassword, string digestedPassword, string authenticationUrl);
-        Task Logout();
-    }
+   public interface IAuthenticationService
+   {
+      User User { get; }
+      Task Initialize();
+      Task<bool> Login(string username, string plaintextPassword, string digestedPassword, string authenticationUrl);
+      Task Logout();
+   }
 
-    public class AuthenticationService : IAuthenticationService
-    {
-        private readonly IHttpService _httpService;
-        private readonly NavigationManager _navigationManager;
-        private readonly ILocalStorageService _localStorageService;
+   public class AuthenticationService : IAuthenticationService
+   {
+      private readonly IHttpService _httpService;
+      private readonly NavigationManager _navigationManager;
+      private readonly ILocalStorageService _localStorageService;
 
-        public User User { get; private set; }
+      public User User { get; private set; }
 
-        public AuthenticationService(
-            IHttpService httpService,
-            NavigationManager navigationManager,
-            ILocalStorageService localStorageService
-        ) {
-            _httpService = httpService;
-            _navigationManager = navigationManager;
-            _localStorageService = localStorageService;
-        }
+      public AuthenticationService(
+          IHttpService httpService,
+          NavigationManager navigationManager,
+          ILocalStorageService localStorageService
+      )
+      {
+         _httpService = httpService;
+         _navigationManager = navigationManager;
+         _localStorageService = localStorageService;
+      }
 
-        public async Task Initialize()
-        {
-            User = await _localStorageService.GetItem<User>("user");
-        }
+      public async Task Initialize()
+      {
+         User = await _localStorageService.GetItem<User>("user");
+      }
 
-        public async Task<bool> Login(string username, string plaintextPassword, string digestedPassword, string authenticationUrl)
-        {
-            var authResult = await _httpService.Post<UserAuthenticateResponse>(authenticationUrl, new AuthenticateUserRequest(username, digestedPassword));
-            if (authResult.Status != Contracts.Enum.ResponseCode.Success)
-            {
-                return false;
-            }
+      public async Task<bool> Login(string username, string plaintextPassword, string digestedPassword, string authenticationUrl)
+      {
+         var (_, payload) = await _httpService.Post<UserAuthenticateResponse>(authenticationUrl, new AuthenticateUserRequest(username, digestedPassword));
+         var authResult = payload;
+         if (authResult.Status != Contracts.Enum.ResponseCode.Success)
+         {
+            return false;
+         }
 
-            User = new User(authResult.Id, authResult.Token);
+         User = new User(authResult.Id, authResult.Token);
 
-            if (string.IsNullOrEmpty(authResult.EncryptedPrivateKey))
-            {
-                User.PrivateKey = null;
-            }
-            else
-            {
-                var decryptionKey = CryptoLib.Common.CreateSymmetricKeyFromUserDetails(username, plaintextPassword, authResult.Id);
-                byte[] decodedPrivateKey = Convert.FromBase64String(authResult.EncryptedPrivateKey);
-                byte[] decryptedPrivateKey = CryptoLib.Common.UndoSymmetricEncryption(decodedPrivateKey, decryptionKey);
-                User.PrivateKey = Encoding.UTF8.GetString(decryptedPrivateKey);
-            }
+         if (string.IsNullOrEmpty(authResult.EncryptedPrivateKey))
+         {
+            User.PrivateKey = null;
+         }
+         else
+         {
+            var decryptionKey = CryptoLib.Common.CreateSymmetricKeyFromUserDetails(username, plaintextPassword, authResult.Id);
+            byte[] decodedPrivateKey = Convert.FromBase64String(authResult.EncryptedPrivateKey);
+            byte[] decryptedPrivateKey = CryptoLib.Common.UndoSymmetricEncryption(decodedPrivateKey, decryptionKey);
+            User.PrivateKey = Encoding.UTF8.GetString(decryptedPrivateKey);
+         }
 
-            await _localStorageService.SetItem("user", User);
-            return true;
-        }
+         await _localStorageService.SetItem("user", User);
+         return true;
+      }
 
-        public async Task Logout()
-        {
-            User = null;
-            await _localStorageService.RemoveItem("user");
-            _navigationManager.NavigateTo("/", true);
-        }
-    }
+      public async Task Logout()
+      {
+         User = null;
+         await _localStorageService.RemoveItem("user");
+         _navigationManager.NavigateTo("/", true);
+      }
+   }
 }
--- Crypter.Web/Services/HttpService.cs
+++ Crypter.Web/Services/HttpService.cs
@@ -9,82 +9,83 @@ using System.Threading.Tasks;
 
 namespace Crypter.Web.Services
 {
-    public interface IHttpService
-    {
-        Task<T> Get<T>(string uri, bool withAuthorization = false);
-        Task<T> Post<T>(string uri, object value, bool withAuthorization = false);
-    }
+   public interface IHttpService
+   {
+      Task<(HttpStatusCode HttpStatus, T Payload)> Get<T>(string uri, bool withAuthorization = false);
+      Task<(HttpStatusCode HttpStatus, T Payload)> Post<T>(string uri, object value, bool withAuthorization = false);
+   }
 
-    public class HttpService : IHttpService
-    {
-        private readonly HttpClient _httpClient;
-        private readonly NavigationManager _navigationManager;
-        private readonly ILocalStorageService _localStorageService;
+   public class HttpService : IHttpService
+   {
+      private readonly HttpClient _httpClient;
+      private readonly NavigationManager _navigationManager;
+      private readonly ILocalStorageService _localStorageService;
 
-        public HttpService(
-            HttpClient httpClient,
-            NavigationManager navigationManager,
-            ILocalStorageService localStorageService
-        ) {
-            _httpClient = httpClient;
-            _navigationManager = navigationManager;
-            _localStorageService = localStorageService;
-        }
+      public HttpService(
+          HttpClient httpClient,
+          NavigationManager navigationManager,
+          ILocalStorageService localStorageService
+      )
+      {
+         _httpClient = httpClient;
+         _navigationManager = navigationManager;
+         _localStorageService = localStorageService;
+      }
 
-        public async Task<T> Get<T>(string uri, bool withAuthorization)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, uri);
-            return await SendRequest<T>(request, withAuthorization);
-        }
+      public async Task<(HttpStatusCode HttpStatus, T Payload)> Get<T>(string uri, bool withAuthorization)
+      {
+         var request = new HttpRequestMessage(HttpMethod.Get, uri);
+         return await SendRequest<T>(request, withAuthorization);
+      }
 
-        public async Task<T> Post<T>(string uri, object value, bool withAuthorization)
-        {
-            var request = new HttpRequestMessage(HttpMethod.Post, uri)
+      public async Task<(HttpStatusCode HttpStatus, T Payload)> Post<T>(string uri, object value, bool withAuthorization)
+      {
+         var request = new HttpRequestMessage(HttpMethod.Post, uri)
+         {
+            Content = JsonContent.Create(value)
+         };
+         return await SendRequest<T>(request, withAuthorization);
+      }
+
+      private async Task<(HttpStatusCode HttpStatus, T Payload)> SendRequest<T>(HttpRequestMessage request, bool withAuthorization)
+      {
+
+         if (withAuthorization)
+         {
+            var user = await _localStorageService.GetItem<User>("user");
+            if (user == null)
             {
-                Content = JsonContent.Create(value)
-            };
-            return await SendRequest<T>(request, withAuthorization);
-        }
-
-        private async Task<T> SendRequest<T>(HttpRequestMessage request, bool withAuthorization)
-        {
-            
-            if (withAuthorization)
-            {
-                var user = await _localStorageService.GetItem<User>("user");
-                if (user == null)
-                {
-                    return HandleMissingUser<T>();
-                }
-
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", user.Token);
+               return HandleMissingUser<T>();
             }
 
-            HttpResponseMessage response;
-            try
-            {
-                response = await _httpClient.SendAsync(request);
-            }
-            catch (Exception)
-            {
-                return default;
-            }
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", user.Token);
+         }
 
-            if (response.StatusCode == HttpStatusCode.Unauthorized)
-            {
-                return HandleMissingUser<T>();
-            }
+         HttpResponseMessage response;
+         try
+         {
+            response = await _httpClient.SendAsync(request);
+         }
+         catch (Exception)
+         {
+            return (HttpStatusCode.ServiceUnavailable, default);
+         }
 
-            T content = await response.Content.ReadFromJsonAsync<T>();
-            response.Dispose();
+         if (response.StatusCode == HttpStatusCode.Unauthorized)
+         {
+            return HandleMissingUser<T>();
+         }
 
-            return content;
-        }
+         T content = await response.Content.ReadFromJsonAsync<T>();
+         response.Dispose();
 
-        private T HandleMissingUser<T>()
-        {
-            _navigationManager.NavigateTo("/", true);
-            return default;
-        }
-    }
+         return (response.StatusCode, content);
+      }
+
+      private (HttpStatusCode HttpStatus, T Payload) HandleMissingUser<T>()
+      {
+         _navigationManager.NavigateTo("/", true);
+         return (HttpStatusCode.Unauthorized, default);
+      }
+   }
 }
