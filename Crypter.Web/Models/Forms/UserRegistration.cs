4b9268ae2efd458bf46f599b7e569c6fb82b2291
Jack-Edwards <jackedwards@protonmail.com>
2021-09-26 15:14:54 -0500

    Implement custom validation for user registration

---
+++ Crypter.Web/Models/Forms/UserRegistration.cs
@@ -0,0 +1,11 @@
+﻿namespace Crypter.Web.Models.Forms
+{
+   public class UserRegistration
+   {
+      public string Username { get; set; }
+      public string Password { get; set; }
+      public string PasswordConfirm { get; set; }
+      public string EmailAddress { get; set; }
+      public string BetaKey { get; set; }
+   }
+}
