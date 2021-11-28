52102ffa0bddd0e577ce7d603a0e5d9c2fc8520c
Jack-Edwards <jackedwards@protonmail.com>
2021-11-27 21:42:49 -0600

    Undo tokenDuration change

--- Crypter.Contracts/Responses/User/UserAuthenticationRefreshResponse.cs
+++ Crypter.Contracts/Responses/User/UserAuthenticationRefreshResponse.cs
@@ -6,13 +6,11 @@ namespace Crypter.Contracts.Responses
    public class UserAuthenticationRefreshResponse
    {
       public string Token { get; set; }
-      public TimeSpan TokenDuration { get; set; }
 
       [JsonConstructor]
-      public UserAuthenticationRefreshResponse(string token, TimeSpan tokenDuration)
+      public UserAuthenticationRefreshResponse(string token)
       {
          Token = token;
-         TokenDuration = tokenDuration;
       }
    }
 }
