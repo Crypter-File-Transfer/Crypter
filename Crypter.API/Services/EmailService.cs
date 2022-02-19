1d37e5ca5d5aa88258735d95e303b82d128f8d66
Jack-Edwards <jackedwards@protonmail.com>
2022-02-18 19:06:29 -0600

    Attempt to cleanup Authentication controller with new patterns

--- Crypter.API/Services/EmailService.cs
+++ Crypter.API/Services/EmailService.cs
@@ -24,7 +24,7 @@
  * Contact the current copyright holder to discuss commerical license options.
  */
 
-using Crypter.API.Controllers.Methods;
+using Crypter.API.Methods;
 using Crypter.API.Models;
 using Crypter.Common.Services;
 using Crypter.Contracts.Common.Enum;
