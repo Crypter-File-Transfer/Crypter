5d30a96b28906f639b8e9a60b6f4f73c0ff2c839
Jack-Edwards <jackedwards@protonmail.com>
2022-12-21 12:20:53 -0600

    Use JSImportAttribute for file downloads

--- Crypter.Web/wwwroot/js/scripts.js
+++ Crypter.Web/wwwroot/js/scripts.js
@@ -37,53 +37,5 @@
    CollapseNavBar: function () {
       var nav = document.getElementById('mainNavigation');
       nav.classList.remove('show');
-   },
-
-   DownloadFile: {
-      _downloadFileBuffer: new Array(),
-      _downloadFileBufferIndex: 0,
-      _downloadBlob: null,
-      _downloadLink: null,
-      _downloadUrl: "",
-
-      InitializeBuffer: function (size) {
-         this._downloadFileBuffer = new Array(size);
-         this._downloadFileBufferIndex = 0;
-      },
-
-      ClearBuffer: function () {
-         this._downloadFileBuffer = new Array();
-         this._downloadFileBufferIndex = 0;
-      },
-
-      InsertBuffer: function (bytes) {
-         this._downloadFileBuffer[this._downloadFileBufferIndex] = bytes;
-         this._downloadFileBufferIndex++;
-      },
-
-      Download: function (fileName, contentType) {
-         this._downloadBlob = new Blob(this._downloadFileBuffer, { type: contentType });
-         this.ClearBuffer();
-
-         this._downloadLink = document.createElement('a');
-         this._downloadLink.download = fileName;
-         this._downloadLink.target = '_self';
-         this._downloadLink.style.display = 'none';
-
-         this._downloadUrl = URL.createObjectURL(this._downloadBlob);
-         this._downloadLink.href = this._downloadUrl;
-         document.body.appendChild(this._downloadLink);
-         this._downloadLink.click();
-      },
-
-      ResetDownload: function () {
-         try {
-            URL.revokeObjectURL(this._downloadUrl);
-            document.body.removeChild(this._downloadLink);
-         } catch { }
-         this._downloadBlob = null;
-         this._downloadLink = null;
-         this._downloadUrl = "";
-      }
    }
 }
