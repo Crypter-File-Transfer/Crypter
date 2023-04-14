3e91e4b5f8557e492c716554c63ace6a9d04a7ae
Jack-Edwards <jackedwards@protonmail.com>
2023-04-13 19:11:09 -0500

    Rename database schema and use 'DEFAULT_PASSWORD' as default database password

--- Containers/PostgreSQL/postgres-init-files/init.sh
+++ Containers/PostgreSQL/postgres-init-files/init.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-   CREATE DATABASE crypter;
+# Create hangfire database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
    CREATE DATABASE crypter_hangfire;
 EOSQL
 
+# Create crypter_user
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-   CREATE USER crypter_user WITH PASSWORD 'CHANGE_ME';
-   REVOKE ALL PRIVILEGES ON DATABASE postgres FROM crypter_user;
-   REVOKE ALL PRIVILEGES ON SCHEMA public FROM crypter_user;
-   REVOKE CREATE ON SCHEMA public FROM PUBLIC;
-EOSQL
+   CREATE USER crypter_user WITH PASSWORD 'DEFAULT_PASSWORD';
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "crypter" <<-EOSQL
-   REVOKE ALL PRIVILEGES ON DATABASE crypter FROM crypter_user;
-   REVOKE ALL PRIVILEGES ON SCHEMA public FROM crypter_user;
-   REVOKE CREATE ON SCHEMA public FROM PUBLIC;
+   REVOKE ALL PRIVILEGES ON DATABASE crypter_hangfire FROM crypter_user;
+
    GRANT CONNECT ON DATABASE crypter TO crypter_user;
-   ALTER DEFAULT PRIVILEGES FOR USER postgres IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO crypter_user;
+   CREATE SCHEMA IF NOT EXISTS crypter AUTHORIZATION crypter_user;
+   GRANT CREATE ON DATABASE crypter TO crypter_user;
+   GRANT CREATE ON SCHEMA public TO crypter_user;
+   GRANT CREATE ON SCHEMA crypter TO crypter_user;
+   GRANT USAGE ON SCHEMA public TO crypter_user;
+   GRANT USAGE ON SCHEMA crypter TO crypter_user;
 EOSQL
 
+# Create crypter_hangfire_user
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "crypter_hangfire" <<-EOSQL
-   CREATE USER crypter_hangfire_user WITH PASSWORD 'CHANGE_ME';
-   REVOKE ALL PRIVILEGES ON DATABASE crypter FROM crypter_hangfire_user;
-   REVOKE ALL PRIVILEGES ON SCHEMA public FROM crypter_hangfire_user;
+   CREATE USER crypter_hangfire_user WITH PASSWORD 'DEFAULT_PASSWORD';
 
-   CREATE SCHEMA IF NOT EXISTS Hangfire AUTHORIZATION crypter_hangfire_user;
-   GRANT CREATE ON schema Hangfire TO crypter_hangfire_user;
+   REVOKE ALL PRIVILEGES ON DATABASE crypter FROM crypter_hangfire_user;
+
+   GRANT CONNECT ON DATABASE crypter_hangfire TO crypter_hangfire_user;
+   CREATE SCHEMA IF NOT EXISTS hangfire AUTHORIZATION crypter_hangfire_user;
+   GRANT CREATE ON DATABASE crypter_hangfire TO crypter_hangfire_user;
+   GRANT CREATE ON SCHEMA public TO crypter_hangfire_user;
+   GRANT CREATE ON SCHEMA hangfire TO crypter_hangfire_user;
+   GRANT USAGE ON SCHEMA public TO crypter_hangfire_user;
+   GRANT USAGE ON SCHEMA hangfire TO crypter_hangfire_user;
 EOSQL
