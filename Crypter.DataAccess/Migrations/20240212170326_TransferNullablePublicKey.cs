ce84e3c30b4e27820ff1d2190541cd5df287dda7
Jack-Edwards <jackedwards@protonmail.com>
2024-02-12 11:11:18 -0600

    Add migration applying "nullable" changes to entities

---
+++ Crypter.DataAccess/Migrations/20240212170326_TransferNullablePublicKey.cs
@@ -0,0 +1,358 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Crypter.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class TransferNullablePublicKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "crypter",
+                table: "UserToken",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "VerificationKey",
+                schema: "crypter",
+                table: "UserRecovery",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                schema: "crypter",
+                table: "UserProfile",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Alias",
+                schema: "crypter",
+                table: "UserProfile",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "About",
+                schema: "crypter",
+                table: "UserProfile",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "KeyExchangeNonce",
+                schema: "crypter",
+                table: "UserMessageTransfer",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RecoveryProof",
+                schema: "crypter",
+                table: "UserMasterKey",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PublicKey",
+                schema: "crypter",
+                table: "UserKeyPair",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PrivateKey",
+                schema: "crypter",
+                table: "UserKeyPair",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Nonce",
+                schema: "crypter",
+                table: "UserKeyPair",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "KeyExchangeNonce",
+                schema: "crypter",
+                table: "UserFileTransfer",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "VerificationKey",
+                schema: "crypter",
+                table: "UserEmailVerification",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                schema: "crypter",
+                table: "User",
+                type: "citext",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "citext",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                schema: "crypter",
+                table: "User",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordHash",
+                schema: "crypter",
+                table: "User",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "KeyExchangeNonce",
+                schema: "crypter",
+                table: "AnonymousMessageTransfer",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "KeyExchangeNonce",
+                schema: "crypter",
+                table: "AnonymousFileTransfer",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "crypter",
+                table: "UserToken",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "VerificationKey",
+                schema: "crypter",
+                table: "UserRecovery",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                schema: "crypter",
+                table: "UserProfile",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Alias",
+                schema: "crypter",
+                table: "UserProfile",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "About",
+                schema: "crypter",
+                table: "UserProfile",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "KeyExchangeNonce",
+                schema: "crypter",
+                table: "UserMessageTransfer",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RecoveryProof",
+                schema: "crypter",
+                table: "UserMasterKey",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PublicKey",
+                schema: "crypter",
+                table: "UserKeyPair",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PrivateKey",
+                schema: "crypter",
+                table: "UserKeyPair",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Nonce",
+                schema: "crypter",
+                table: "UserKeyPair",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "KeyExchangeNonce",
+                schema: "crypter",
+                table: "UserFileTransfer",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "VerificationKey",
+                schema: "crypter",
+                table: "UserEmailVerification",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                schema: "crypter",
+                table: "User",
+                type: "citext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "citext");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                schema: "crypter",
+                table: "User",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordHash",
+                schema: "crypter",
+                table: "User",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "KeyExchangeNonce",
+                schema: "crypter",
+                table: "AnonymousMessageTransfer",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "KeyExchangeNonce",
+                schema: "crypter",
+                table: "AnonymousFileTransfer",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+        }
+    }
+}
